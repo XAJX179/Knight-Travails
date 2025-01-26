c67fa9e1f46d6e5c33596caaa9819acd1f243589
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2025-01-26 19:57:55 +0530

    chore: update main.rb

--- main.rb
+++ main.rb
@@ -4,4 +4,6 @@ require_relative 'lib/knight_travails'
 
 game = KnightTravails.new
 
-game.knight_moves([2, 1], [3, 3]) #  == [[0,0],[2,1],[3,3]]  or  [[0,0],[1,2],[3,3]]
+game.knight_moves([0, 0], [3, 3]) #  == [[0,0],[2,1],[3,3]]  or  [[0,0],[1,2],[3,3]]
+
+game.knight_moves([3, 3], [0, 0])
